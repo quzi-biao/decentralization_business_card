89da269d7786408a977e40c77034e5c2adb5d9f5
xie zhengbiao <450695496@qq.com>
2026-01-17 00:45:21 +0800

    feat: 修复两个报错

--- App.tsx
+++ App.tsx
@@ -89,7 +89,6 @@ export default function App() {
               marginTop: 4,
             },
             lazy: false,
-            detachInactiveScreens: false,
           }}
         >
           <Tab.Screen
